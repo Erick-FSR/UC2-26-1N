0085eee29620272fda0f7adddc3c937e184ef6e8
Erick-FSR <erick.f.rodrigues0@gmail.com>
2026-03-26 21:42:18 -0300

    Add files via upload

---
+++ 19-03/testandoTypeOf.js
@@ -0,0 +1,10 @@
+/*const myAge = 19;
+const myAgeText = myAge.toString()
+
+console.log(myAge, typeof myAge, myAgeText, typeof myAgeText)*/
+
+let primeiroValor = 10
+let segundoValor = 10
+
+let resultado = primeiroValor * segundoValor;
+console.log(resultado)
