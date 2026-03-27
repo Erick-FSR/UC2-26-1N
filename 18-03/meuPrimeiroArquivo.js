574e8dcda831bde9925a7efafd525879c7d25844
Erick-FSR <erick.f.rodrigues0@gmail.com>
2026-03-26 21:42:36 -0300

    Add files via upload

---
+++ 18-03/meuPrimeiroArquivo.js
@@ -0,0 +1,20 @@
+/*let novaVariavel = 100;
+console.log (novaVariavel);
+
+novaVariavel = "Leonardo";
+console.log(novaVariavel);
+
+const nome = "Dionatan";
+const idade = 33;
+
+console.log ("Olá!", "Meu nome é", nome, "e tenho", idade, "anos" );*/
+
+/*let minhaVariavel = "Leonardo 25";
+console.log(minhaVariavel);*/
+
+let primeiroNome = "Erick";
+let segundoNome = "Fernando";
+let idade = 19;
+let souumEstudante = true;
+
+console.log ("Olá!", "Meu nome é", primeiroNome, segundoNome, "e tenho", idade, "anos.", "Sou estudante:", souumEstudante)l
